04879de3aad297e05ae9cc7a73100aa4ebe53865
thomas-abraham13 <thomasabraham832@gmail.com>
2023-12-13 00:16:49 +0530

    added stuff

--- joey.js
+++ joey.js
@@ -1,14 +1,27 @@
-// Script Name: {Python (WebDriver)}
+// Script Name: Joey Easter Egg
 
 const puppeteer = require('puppeteer');
 
 (async () => {
-const browser = await puppeteer.launch({ headless: false, defaultViewport: { width: 1920, height: 1080 }, args: ['--start-maximized'] });
+const browser = await puppeteer.launch({ headless: true, defaultViewport: { width: 1920, height: 1080 }, args: ['--start-maximized'] });
 const page = await browser.newPage();
 let element, formElement, tabs;
 
 await page.goto(`https://www.google.com/search?q=joey+tribbiani`, { waitUntil: 'networkidle0' });
 element = await page.$x(`//div[@id='rcnt']/div[2]/div/div/div[3]/div/div/div/div/div/div/div/canvas`);
 	await element[0].click();
+
+console.log("Waiting for 3 seconds");
+await delay(3000);
+console.log("Waiting for 3 seconds : SUCCESS");
+
 await browser.close();
+
+//JavaScript delay function example
+function delay(time) {
+	return new Promise(function(resolve) { 
+		setTimeout(resolve, time)
+	});
+}
+
 })();
